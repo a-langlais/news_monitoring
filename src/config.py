964156f2a660410794540812556e1e249451323d
Alexandre LANGLAIS <160505900+a-langlais@users.noreply.github.com>
2025-04-14 20:37:29 +0200

    Add more RSS

--- src/config.py
+++ src/config.py
@@ -14,13 +14,16 @@ SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 
 ### --- RSS Feeds --- ###
 RSS_FEEDS = [
-    "https://www.databricks.com/fr/feed",   # Databricks
-    "https://hf.co/blog/feed.xml",          # Hugging-Face Blog
-    "https://jamesg.blog/hf-papers.xml",    # Hugging-Face Papers
-    "theverge.com/rss/index.xml",           # The Verge
-    "http://www.kdnuggets.com/feed"         # KDnuggets
-    "https://techcrunch.com/feed/",         # TechCrunch
-    "https://www.cnil.fr/fr/rss.xml",       # CNIL
+    "https://www.databricks.com/fr/feed",       # Databricks
+    "hhttps://blogs.microsoft.com/AI/feed/",    # Microsoft AI Blog
+    "https://hf.co/blog/feed.xml",              # Hugging-Face Blog
+    "https://jamesg.blog/hf-papers.xml",        # Hugging-Face Papers
+    "theverge.com/rss/index.xml",               # The Verge
+    "http://www.kdnuggets.com/feed"             # KDnuggets
+    "https://techcrunch.com/feed/",             # TechCrunch
+    "https://towardsdatascience.com/feed",      # Towards Data Science
+    "https://www.cnil.fr/fr/rss.xml",           # CNIL
+    "https://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml",      # ScienceDaily
 ]
 
 ### --- Autres paramètres --- ###
